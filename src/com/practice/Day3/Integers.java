d447d700b7609129c271d91edcac210165106114
Rushikesh Here <rushikeshhere@gmail.com>
2023-03-22 10:49:31 +0530

    The First 10 Integers are

---
+++ src/com/practice/Day3/Integers.java
@@ -0,0 +1,14 @@
+package com.practice.Day3;
+
+public class Integers {
+
+	public static void main(String[] args) {
+		int c;
+		
+		System.out.println("The first 10 integers are :- ");
+		
+		for (c = 0; c <= 10; c++) {
+			System.out.println(c);
+		}
+	}
+}
