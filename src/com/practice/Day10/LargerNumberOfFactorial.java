51595da89f9f61f228f720e3fbdf3ea51a2c542f
Rushikesh Here <rushikeshhere@gmail.com>
2023-03-25 13:38:25 +0530

    Calculating Factorial

---
+++ src/com/practice/Day10/LargerNumberOfFactorial.java
@@ -0,0 +1,26 @@
+package com.practice.Day10;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class LargerNumberOfFactorial {
+
+	public static void main(String[] args) {
+
+		int number;
+		BigInteger inc = new BigInteger("1");
+		BigInteger fact = new BigInteger("1");
+
+		Scanner sc = new Scanner(System.in);
+
+		number = sc.nextInt();
+
+		for(int i=1;number>=i;i++)
+		{
+			fact=fact.multiply(inc);
+			inc=inc.add(BigInteger.ONE);
+		}
+		System.out.println(fact);
+	}
+
+}
---
+++ src/com/practice/Day11/ForLoop.java
@@ -0,0 +1,12 @@
+package com.practice.Day11;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		int number = 10;
+
+		for (int i = 1; i <= number; ++i) {
+			System.out.println(i);
+		}
+	}
+}
