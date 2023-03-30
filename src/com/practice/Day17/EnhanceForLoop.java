be7d2c7b5a24a2e1931f04baceeb437c7d98ae57
Rushikesh Here <rushikeshhere@gmail.com>
2023-03-30 12:49:59 +0530

    getting numbers for the array

--- src/com/practice/Day17/EnhanceForLoop.java
+++ src/com/practice/Day17/EnhanceForLoop.java
@@ -4,7 +4,6 @@ public class EnhanceForLoop {
 	public static void main(String[] args) {
 
 		int numbers[] = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
-
 		for (int t : numbers) {
 			System.out.println(t);
 		}
