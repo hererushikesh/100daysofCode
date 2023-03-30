7ae763ed7cb49fd8c1fe48001cc70703b06eda8d
Rushikesh Here <rushikeshhere@gmail.com>
2023-03-30 12:51:37 +0530

    Geeting Strings from the array

--- src/com/practice/Day17/EnhanceForLoop1.java
+++ src/com/practice/Day17/EnhanceForLoop1.java
@@ -4,14 +4,8 @@ public class EnhanceForLoop1 {
 	public static void main(String[] args) {
 		String languages[]= {"English","marathi","Hindi","Tamil","etc..."};
 		
-		// Enhance For loop
-//		for(String l:languages) {
-//			System.out.println(l);
-//		}
-		
-		// General for loop
-		for(int i=0;i<=languages.length-1;i++) {
-			System.out.println(languages[i]);
+		for(String l:languages) {
+			System.out.println(l);
 		}
 	}
 }
