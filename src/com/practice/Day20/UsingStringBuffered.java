d834fc06c45cff64d2e08a97aca2a4b146e276cf
Rushikesh Here <rushikeshhere@gmail.com>
2023-04-05 12:18:01 +0530

    Reverse String using StringBufferClass

---
+++ src/com/practice/Day20/UsingStringBuffered.java
@@ -0,0 +1,8 @@
+package com.practice.Day20;
+
+public class UsingStringBuffered {
+	public static void main(String[] args) {
+		StringBuffer stringBuffer = new StringBuffer("madaM");
+		System.out.println(stringBuffer.reverse());
+	}
+}
