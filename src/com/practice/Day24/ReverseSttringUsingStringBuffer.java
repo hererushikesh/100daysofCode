f65816a2303cf063a046bcc18d14e0358feae950
Rushikesh here <rushikeshhere@gmail.com>
2023-04-26 12:17:10 +0530

    Reverse Sttring using stringBuffer

---
+++ src/com/practice/Day24/ReverseSttringUsingStringBuffer.java
@@ -0,0 +1,10 @@
+package com.practice.Day24;
+
+public class ReverseSttringUsingStringBuffer {
+
+	public static void main(String[] args) {
+
+		StringBuffer sbf = new StringBuffer("Rushikesh here");
+		System.out.println(sbf.reverse());
+	}
+}
